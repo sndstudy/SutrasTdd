b464e7e33529f27abcc530eee971f9929dd25d32
sndstudy <sanada2054ss@gmail.com>
2018-01-13 23:44:03 +0900

    テスト駆動開発 79ページまで実装

--- src/test/java/sndstudy/tdd/test/MoneyTest.java
+++ src/test/java/sndstudy/tdd/test/MoneyTest.java
@@ -1,7 +1,6 @@
 package sndstudy.tdd.test;
 
 import org.junit.Test;
-import sndstudy.tdd.Franc;
 import sndstudy.tdd.Money;
 
 import static org.junit.Assert.*;
@@ -23,19 +22,7 @@ public class MoneyTest {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-
-    }
-
-    @Test
-    public void testFrancMultiplication(){
-
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
 
     }
 
@@ -45,11 +32,6 @@ public class MoneyTest {
         assertEquals("USD",Money.dollar(1).currency());
         assertEquals("CHF",Money.franc(1).currency());
 
-    }
-
-    @Test
-    public void testDifferentClassEquals(){
-        assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
     }
 
 }
